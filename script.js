42093886ce14429eb030541ccd8d95dd1ade933e
shivraj0002 <95290113+shivraj0002@users.noreply.github.com>
2023-03-10 00:03:37 +0530

    StopWatch UI complete

---
+++ script.js
@@ -0,0 +1,17 @@
+const hoursInput = document.getElementById("hours-input");
+const minutesInput = document.getElementById("minuts-input");
+const secondsInput = document.getElementById("seconds-input");
+
+
+const hoursOutput = document.getElementById("hours-output");
+const minutesOutput = document.getElementById("minuts-output");
+const secondsOutput = document.getElementById("seconds-output");
+
+const startBtn = document.getElementById("start-btn");
+const infiniteBtn = document.getElementById("infinite-btn");
+const stoptBtn = document.getElementById("stop-btn");
+const resettBtn = document.getElementById("reset-btn");
+
+
+
+console.log(minutesOutput);
